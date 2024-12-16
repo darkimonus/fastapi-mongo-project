dc1515b3742e8e10a9968668812b6d45bdc318d2
kimyaroshenko <kim.yaroshenko@onix-systems.com>
2024-12-16 16:29:30 +0200

    feature(auth): added twilio

--- source/auth/tasks.py
+++ source/auth/tasks.py
@@ -1,5 +1,16 @@
-def send_auth_code(phone: str):
-    """
-    This function should send sms code by mobile phone for user authentication.
-    """
-    pass
+from twilio.rest import Client
+from conf import config
+from custom_logging.middleware import logger
+
+account_sid = config.get('TWILIO_SID')
+auth_token = config.get('TWILIO_SECRET')
+client = Client(account_sid, auth_token)
+
+
+def send_code(phone_number: str, code: str):
+    message = client.messages.create(
+        body=f'Your verification code is {code}',
+        from_='+1234567890',
+        to=phone_number
+    )
+    return message
